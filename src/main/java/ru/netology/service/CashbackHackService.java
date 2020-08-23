f56936e4cd9929062c9db21294d46f1f24de7aba
Timur Kiselev <snygens@mail.ru>
2020-08-23 13:25:18 +0300

    add tests

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,9 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        return boundary - amount % boundary;
+        if (amount != 1000 & amount > 0) {
+            return boundary - amount % boundary;
+        }
+        else return 0;
     }
 }
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,11 +5,23 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @org.testng.annotations.Test
-    public void testRemain() {
-        final int boundary = 1000;
-        int amountSumm = 860;
+    public void shouldNegativeRemain() {
         CashbackHackService cashback = new CashbackHackService();
-        int expected = cashback.remain(amountSumm) % boundary;
-        assertEquals(140, expected);
+        int actual = cashback.remain(-800);
+        assertEquals(actual, 0);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldremain() {
+        CashbackHackService cashback = new CashbackHackService();
+        int actual = cashback.remain(800);
+        assertEquals(actual, 200);
+    }
+
+    @org.testng.annotations.Test
+    public void remain() {
+        CashbackHackService cashback = new CashbackHackService();
+        int actual = cashback.remain(1000);
+        assertEquals(actual, 0);
     }
 }
