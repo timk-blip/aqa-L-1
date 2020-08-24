8a9e04e3f53719ba0787f90726d172bc869c6a67
Timur Kiselev <snygens@mail.ru>
2020-08-24 12:22:00 +0300

    fixed remarks

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,9 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (amount != 1000 & amount > 0) {
             return boundary - amount % boundary;
-        }
-        else return 0;
     }
 }
